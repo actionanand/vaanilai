cb9d01c780fe3335e9d73c8824d0e223f1138ca7
actionanand <meetanandns@gmail.com>
2019-12-28 00:06:02 +0530

    working with dark sky

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/c5f2c915e55c4a57ecb2387f527a0716/37.8267,-122.4233';
+
+request({url: url}, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.currently);
+});
