228a472e1429317ee0a6b7b68cd476573e86d044
actionanand <meetanandns@gmail.com>
2019-12-30 21:17:09 +0530

    refactoring the code

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const request = require('request');
 const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
+const address = process.argv[2];
 
-geoCode('nagercoil', (error, data) => {
-    if (error) {
-        return console.log(error);
-    }
-    forecast(data.lat, data.lng, (error, respData) => {
+if (!address) {
+    console.log('Please provide an address!')
+} else {
+    geoCode(address, (error, data) => {
         if (error) {
-            console.log(error);
+            return console.log(error);
         }
-        console.log(data.location);
-        console.log(respData);
+        forecast(data.lat, data.lng, (error, respData) => {
+            if (error) {
+                console.log(error);
+            }
+            console.log(data.location);
+            console.log(respData);
+        });
     });
-});
+}
